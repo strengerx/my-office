4656cbf73b2aba5416f4c42a38e43039c70e4dbd
Bidyut Hazra <bidyut.hrz74@gmail.com>
2023-11-27 22:13:33 +0530

    change to new branch

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,12 @@
 const Home = () => {
       return (<>
             <h1>This Is Home Page</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad rerum unde tempora harum, explicabo doloremque esse. Dolores, commodi? Optio qui placeat quisquam ea aspernatur quidem quasi quo beatae asperiores dolore.</p>
+            <ul>
+                  <li>Home</li>
+                  <li>About Us</li>
+                  <li>Contact Us</li>
+            </ul>
       </>);
 }
 
